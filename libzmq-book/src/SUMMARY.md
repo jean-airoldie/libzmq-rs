cee54e39035d96cfb212d8ae85026f66a440c45d
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-05-27 13:21:45 -0400

    Import examples to book

--- libzmq-book/src/SUMMARY.md
+++ libzmq-book/src/SUMMARY.md
@@ -9,6 +9,6 @@
 * [Advanced](./advanced/README.md)
     * [Custom Protocols](./advanced/protocols.md)
 * [Examples](./examples/README.md)
-    * [Basic Server](./examples/basic_server.md)
+    * [Basic Request Reply](./examples/basic_req_rep.md)
 
 [Glossary](./glossary.md)
--- libzmq/examples/basic_req_rep.rs
+++ libzmq/examples/basic_req_rep.rs
@@ -2,10 +2,6 @@ use libzmq::{prelude::*, *};
 
 use std::thread;
 
-// This is a simple as it gets. No send_timeout or
-// recv_timeout, no good error handling. For a `INPROC`
-// server, this works.
-
 fn main() -> Result<(), failure::Error> {
     let addr: InprocAddr = InprocAddr::new_unique();
 
@@ -17,12 +13,7 @@ fn main() -> Result<(), failure::Error> {
     let handle = thread::spawn(move || -> Result<(), failure::Error> {
         loop {
             let request = server.recv_msg()?;
-            // We define a empty message as a termination signal.
-            if request.is_empty() {
-                break Ok(());
-            } else {
-                assert_eq!(request.to_str(), Ok("ping"));
-            }
+            assert_eq!(request.to_str(), Ok("ping"));
 
             // Retrieve the routing_id to route the reply to the client.
             let id = request.routing_id().unwrap();
@@ -41,8 +32,7 @@ fn main() -> Result<(), failure::Error> {
     let msg = client.recv_msg()?;
     assert_eq!(msg.to_str(), Ok("pong"));
 
-    // Send the termination signal.
-    client.send("")?;
+    Ctx::global().shutdown()?;
 
     // Join with the thread.
     handle.join().unwrap()
