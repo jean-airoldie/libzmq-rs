452c6e342bf80557309f0d55659e68ca4adc5a00
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-05-29 17:52:03 -0400

    Added CURVE vs non-CURVE TCP client-server benchmark (#52)
    * Added CURVE vs non-CURVE TCP client-server benchmark
    * Added a book entry about CURVE overhead

--- libzmq/benches/bench_main.rs
+++ libzmq/benches/bench_main.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main};
 
+mod curve;
 mod socket;
 
-criterion_group!(benches, socket::bench);
+criterion_group!(benches, socket::bench, curve::bench);
 criterion_main!(benches);
