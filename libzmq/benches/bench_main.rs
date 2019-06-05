293dde04db58c57d37198aff1345f8fdf0292e7d
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-06-05 19:20:40 -0400

    Added Msg benches

--- libzmq/benches/bench_main.rs
+++ libzmq/benches/bench_main.rs
@@ -2,6 +2,7 @@ use criterion::{criterion_group, criterion_main};
 
 mod curve;
 mod socket;
+mod msg;
 
-criterion_group!(benches, socket::bench, curve::bench);
+criterion_group!(benches, socket::bench, curve::bench, msg::bench);
 criterion_main!(benches);
---
+++ libzmq/benches/msg.rs
@@ -0,0 +1,45 @@
+use criterion::{black_box, Benchmark, Criterion};
+
+use libzmq::Msg;
+
+use std::time::Duration;
+
+pub(crate) fn bench(c: &mut Criterion) {
+    c.bench(
+        &"Msg::from".to_owned(),
+        Benchmark::new("String", move |b| {
+            b.iter(|| {
+                let string = "some string".to_owned();
+                let msg: Msg = string.into();
+                black_box(msg);
+            });
+        })
+        .with_function("&str", move |b| {
+            b.iter(|| {
+                let msg: Msg = "some str".into();
+                black_box(msg);
+            });
+        })
+        .with_function("Vec<u8>", move |b| {
+            b.iter(|| {
+                let bytes = b"some bytes".to_owned();
+                let msg: Msg = bytes.into();
+                black_box(msg);
+            });
+        })
+        .with_function("&[u8; X]", move |b| {
+            b.iter(|| {
+                let msg: Msg = b"some bytes".into();
+                black_box(msg);
+            });
+        })
+        .with_function("&[u8]", move |b| {
+            b.iter(|| {
+                let bytes: &[u8] = b"some bytes";
+                let msg: Msg = bytes.into();
+                black_box(msg);
+            });
+        })
+        .measurement_time(Duration::from_secs(30))
+    );
+}
