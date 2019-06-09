79a7fc65e212fbc9900d62ddbb7d928b2c0ab437
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-06-09 19:22:35 -0400

    Updated gen_curve_cert formatting

--- libzmq/examples/gen_curve_cert.rs
+++ libzmq/examples/gen_curve_cert.rs
@@ -3,5 +3,6 @@ use libzmq::auth::CurveCert;
 // Used to generate `CURVE` certificates.
 fn main() {
     let cert = CurveCert::new_unique();
-    println!("{:#?}", cert);
+    println!("public: \"{}\"", cert.public().as_str());
+    println!("secret: \"{}\"", cert.secret().as_str());
 }
