a4db3c33baecc2f29c178506cb3b4af3396cb7f5
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-06-18 15:03:05 -0400

    Release libzmq 0.1.17

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ they will have to be stabilized before the 1.0 version is released.
 
 ```toml
 [dependencies]
-libzmq = "0.1.16"
+libzmq = "0.1.17"
 ```
 
 # Dead Simple Sample
--- libzmq/src/lib.rs
+++ libzmq/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/libzmq/0.1.16")]
+#![doc(html_root_url = "https://docs.rs/libzmq/0.1.17")]
 
 //! *libzmq* - A strict subset of ØMQ with a high level API.
 
