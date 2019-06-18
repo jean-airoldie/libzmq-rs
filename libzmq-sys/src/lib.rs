7140e760bb8855d02c18caeb820b1551b6c0c5e5
jean-airoldie <25088801+jean-airoldie@users.noreply.github.com>
2019-06-18 15:02:20 -0400

    Release libzmq-sys 0.1.4

--- libzmq-sys/src/lib.rs
+++ libzmq-sys/src/lib.rs
@@ -1,6 +1,6 @@
 // Ignore generated code
 #![allow(clippy::all)]
-#![doc(html_root_url = "https://docs.rs/libzmq-sys/0.1.3")]
+#![doc(html_root_url = "https://docs.rs/libzmq-sys/0.1.4")]
 
 //! libzmq-sys - Raw cFFI bindings to [libzmq](https://github.com/zeromq/libzmq).
 
